e6788b25f9370596633f33dd6646eed9b3575895
Bryan <bryannsanchez1234@gmail.com.com>
2022-05-18 21:59:54 -0700

    changes to home

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,6 +1,5 @@
 import { Link } from 'react-router-dom'
 // import Assets from './Assets'
-
 const Home = () => {
 
 
@@ -12,22 +11,27 @@ const Home = () => {
       <div className='home-cards'>
         <div>
           <div className='home-box1'>
+            <a href='http://localhost:3000/about'>
+              <img className='home-pic' src='/home-pic2.png'></img>
+              </a>
+            </div>
             <div className='home-box1-caption'>
-            <h2 className='name'>Bryan Sanchez</h2>
-            <p2 className='name-quote'>Hello! My name is Bryan & i am a Full-Stack Software Engineer. Welcome to my page where you can learn a little bit about me & view some of my work!</p2>
+            {/* <h2 className='name'>Bryan Sanchez</h2>
+            <h2 className='name-quote'>Hello! My name is Bryan & i am a Full-Stack Software Engineer. Welcome to my page where you can learn a little bit about me & view some of my work!</h2> */}
             <div>
-            <Link to='/about' >About Me</Link>
+            {/* <Link to='/about' >About Me</Link> */}
             </div>
             </div>
-            <img className='home-pic' src='/profile-pic.png'></img>
-          </div>
 
         </div>
         <div className='home-box2'>
+          <h2 className='home-box-title' >Who am i?</h2>
+          <p2 className='text' >Hello! My name is </p2><p2 className='name-'>Bryan Sanchez,</p2>
+          <h2 className='text'>I am a Full-Stack Software Engineer, i </h2>
           <h2 className='home-box-title' >Technologies</h2>
           <div>
-            <div><p2>Front End: HTML, CSS, JavaScript, React, Redux, Vue, Python</p2></div>
-            <div><p3>Back End: Sequelize, Mongoose, Postgres, MongoDB, SQL, PostgreSQL </p3></div>
+            <div className='home-box-caption' ><h2>Front End: HTML, CSS, JavaScript, React, Redux, Vue, Python</h2></div>
+            <div className='home-box-caption'><h2>Back End: Sequelize, Mongoose, Postgres, MongoDB, SQL, PostgreSQL </h2></div>
           </div>
         </div>
 
