29ab8a7d6d4181be0b437eb3cd465dc5abf99133
Bryan <bryannsanchez1234@gmail.com.com>
2022-05-18 23:34:38 -0700

    homepage

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -26,14 +26,26 @@ const Home = () => {
         </div>
         <div className='home-box2'>
           <h2 className='home-box-title' >Who am i?</h2>
-          <p2 className='text' >Hello! My name is </p2><p2 className='name-'>Bryan Sanchez,</p2>
-          <h2 className='text'>I am a Full-Stack Software Engineer, with experience in   </h2>
-          <h2 className='home-box-title' >Technologies</h2>
+          {/* <p2 className='text' >Hello! My name is </p2><p2 className='name-'>Bryan Sanchez,</p2> */}
           <div>
-            <div className='home-box-caption' ><h2>Front End: HTML, CSS, JavaScript, React, Redux, Vue, Python</h2></div>
-            <div className='home-box-caption'><h2>Back End: Sequelize, Mongoose, Postgres, MongoDB, SQL, PostgreSQL </h2></div>
+            <h2 className='text1'>I am a Full-Stack Software Engineer with experience using the latest frameworks availible. I value developing simple & easy to use apps that fit everyones needs.  </h2>
+            <div className='onmymacpic'>
+            <img src='/onmymac.png'></img>
+            </div>
           </div>
         </div>
+
+          <div className='home-box2'>
+            <h2 className='home-box-title'>Technologies</h2>
+            <div className='text2'>
+              <div className='home-box-caption' ><h2>Front End: HTML, CSS, JavaScript, React, Redux, Vue, Python</h2></div>
+
+              <div className='home-box-caption'><h2>Back End: Sequelize, Mongoose, Postgres, MongoDB, SQL, PostgreSQL </h2></div>
+            </div>
+            <div className='light-bulb-pic'>
+              <img className='lbp'src='light-bulb.png'></img>
+            </div>
+          </div>
 
 
 
