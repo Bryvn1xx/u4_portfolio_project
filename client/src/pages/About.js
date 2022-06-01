4507c858f731334b0a3734fa534930db539087ef
Bryan <bryannsanchez1234@gmail.com.com>
2022-06-01 00:36:37 -0700

    mvp

--- client/src/pages/About.js
+++ client/src/pages/About.js
@@ -1,9 +1,17 @@
+import { Link } from 'react-router-dom'
 const About = () => {
   return (
     <div>
-      <h1>About</h1>
+      <h1 className='home-box-title'>About</h1>
       <div className='about-box'>
-        <p className='about-cap'>Hello! My Name is Bryan Sanchez, I am a Full-Stack Software Engineer based out of Los Angeles. I enjoy working with teams & have experience using a wide variety of technologies in team environments. I love being faced with new challenges every-day as they only sharpen my skills. </p>
+        <p className='about-cap-title' >Me</p>
+        <p className='about-cap'>   Hello! My Name is Bryan Sanchez, I am a Full-Stack Software Engineer based out of Los Angeles. I enjoy working with teams & have experience using a wide variety of technologies in team environments. I love being faced with new challenges every-day as they only sharpen my skills. </p>
+        <div className='about-box2'>
+          <a href='https://docs.google.com/document/d/1Gov2U05QAngWiTxTWTEd3XuJ2apUPY6rkDgA16VK8PE/edit?usp=sharing' target='_blank'>
+            <p className='resume-link' >Download Resume Here!</p>
+          </a>
+        </div>
+
         <img className='about-pic' src='/profile-pic.png'></img>
       </div >
     </div >
--- client/src/pages/Projects.js
+++ client/src/pages/Projects.js
@@ -6,7 +6,10 @@ const Projects = () => {
         <div className='proj-container'>
           <div className='proj-1'>
             <a href='https://noderun.herokuapp.com/'>
-            <img className='proj1-img' src='/noderun-screenshot.png'></img>
+              <img className='proj1-img' src='/noderun-screenshot.png'></img>
+            </a>
+            <a className='repos-link' href='https://github.com/Bryvn1xx/u4_capstone_noderun' >
+              <p>Github Repo</p>
             </a>
             <h2>NodeRun</h2>
           </div>
@@ -15,15 +18,24 @@ const Projects = () => {
               <img className='proj1-img' src='/worktrack-app.png'></img>
 
             </a>
+            <a className='repos-link' href='https://github.com/MC-JSON/WorkTrack-FrontEnd/tree/main' >
+              <p>Github Repo</p>
+            </a>
           </div>
           <div className='proj-1'>
             <a href='https://github.com/Bryvn1xx/u2_proj_used_cars'>
               <img className='proj1-img' src='/luxury-motors.png'></img>
             </a>
+            <a className='repos-link' href='https://github.com/Bryvn1xx/u4_capstone_noderun' >
+              <p>Github Repo</p>
+            </a>
           </div>
           <div className='proj-1'>
-            <a href='https://second-birth.surge.sh/'>
+            <a href='https://github.com/Bryvn1xx/connecting4'>
               <img className='proj1-img' src='/connect-4.png'></img>
+            </a>
+            <a className='repos-link' href='https://github.com/Bryvn1xx/u4_capstone_noderun' >
+              <p>Github Repo</p>
             </a>
           </div>
         </div>
